8686f51035baed60f520d0712c4bbb0ce15cc8af
louisdbennett <louis21bennett@gmail.com>
2025-03-12 12:07:35 +0000

    Add cleandat.R

---
+++ cleandat.R
@@ -0,0 +1,104 @@
+rm(list = ls(all = TRUE))
+
+source('funcs.R')
+
+# Load data
+unis <- read.csv("data/finalbis.csv")
+unis$continuation <- as.numeric(unis$continuation)
+# covert POLAR4 %ages to numbers
+unis[, paste0('POLAR4.Q', 1:5)] <- unis[, paste0('POLAR4.Q', 1:5)] * unis$Total
+
+unis$continuation <- as.numeric(unis$continuation) / 100
+unis$satisfied_teaching <- unis$satisfied_teaching / 100
+unis$career_after_15_month <- unis$career_after_15_month / 100
+unis$satisfied_feedback <- unis$satisfied_teaching / 100
+
+
+# Add Ethnic Diversity Index
+c1 <- 100
+c2 <- -100 * sqrt(5 * (5 - 1)) / (5 - 1)
+unis$ethnic_diversity_index <- c1 +
+  c2 *
+    sqrt(
+      (unis$White.ethnic.group - 0.2)^2 +
+        (unis$Black.ethnic.group - 0.2)^2 +
+        (unis$Asian.ethnic.group - 0.2)^2 +
+        (unis$Mixed.ethnic.group - 0.2)^2 +
+        (unis$Other.ethnic.group - 0.2)^2
+    )
+
+# Add Russell Group Uni factor
+unis$russell <- FALSE
+russell_codes <- c(
+  "B32",
+  "B78",
+  "C05",
+  "C15",
+  "D86",
+  "E56",
+  "E84",
+  "G28",
+  "I50",
+  "K60",
+  "L23",
+  "L41",
+  "L72",
+  "M20",
+  "N21",
+  "N84",
+  "O33",
+  "Q50",
+  "Q75",
+  "S18",
+  "S27",
+  "U80",
+  "W20",
+  "Y50"
+)
+unis[unis$INSTITUTION_CODE %in% russell_codes, ]$russell <- TRUE
+
+unis$estimated_gini <- sapply(unis$INSTITUTION_CODE, \(code) {
+  cleaned <- clean_uni(code)
+  q <- estimate_q(cleaned$value, q = 10000)
+  gini(q[-1])
+})
+
+# Task: Predicting graduate employability and the most predictive features
+# Part 1: Basic Linear Regression Model
+
+formula <- career_after_15_month ~
+  Women +
+  russell +
+  ethnic_diversity_index +
+  estimated_gini +
+  continuation +
+  spent_per_student +
+  students_staff_ratio +
+  added_value +
+  avg_entry_tariff +
+  satisfied_teaching + 
+  satisfied_feedback
+
+mod1 <- glm(
+  formula, data = unis, family = quasibinomial
+)
+summary(mod1)
+
+prec.prior <- list(phi = list(prior = "loggamma", param = c(1, 0.01)))
+
+beta.prior <- list(
+  mean.intercept = 0, 
+  prec.intercept = 0.01,
+  mean = 0, 
+  prec = 0.01
+)
+
+inlamod <- INLA::inla(
+  formula = formula,
+  data = unis,
+  family = 'Beta',
+  control.family = list(hyper = prec.prior),
+  control.fixed = beta.prior
+)
+
+summary(inlamod)
